1912040d8375d32461a0bd4aa93c468fe6c84030
nilang <nilang@nilang-X202E.(none)>
2014-04-05 19:40:09 +0530

    test is changed to very no of servers

--- Raft_test.go
+++ Raft_test.go
@@ -1,8 +1,9 @@
 package Raft
 
 import (
+	"errors"
 	"fmt"
-	"log"
+	//"log"
 	"math/rand"
 	"os"
 	"os/exec"
@@ -17,7 +18,7 @@ import (
 
 const (
 	unknown        = 1000
-	unknwon_leader = 0
+	unknown_leader = 0
 )
 
 var wg *sync.WaitGroup
@@ -35,13 +36,14 @@ type LeaderInfo struct {
 
 //start 5 servers and kill randomly 3 servers and after 5 second start them..
 func TestElection_1(t *testing.T) {
+	fmt.Println("test 1")
 	nos = 5
 	cmd = make([]*exec.Cmd, nos)
 	wg = new(sync.WaitGroup)
 	compeletepath := os.Getenv("GOPATH")
 	path := strings.Split(compeletepath, ":")[0] + "/bin/main"
 
-	for i := 1; i < 6; i++ {
+	for i := 1; i < nos+1; i++ {
 		cmd[i-1] = exec.Command(path, "-id", strconv.Itoa(i))
 		cmd[i-1].Start()
 	}
@@ -50,11 +52,11 @@ func TestElection_1(t *testing.T) {
 	case <-time.After(1 * time.Second):
 
 	}
-	for j := 0; j < 5; j = j + 2 {
+	for j := 0; j < nos; j = j + 2 {
 		wg.Add(1)
 
 		//rand.Seed(time.Now().UnixNano())
-		if j == 4 {
+		if j == nos-1 {
 			a = 0
 		} else {
 			a = rand.Intn(2)
@@ -64,27 +66,47 @@ func TestElection_1(t *testing.T) {
 		wg.Wait()
 
 	}
-
 	select {
-	case <-time.After(8 * time.Second):
-		rpc_call()
+	case <-time.After(2 * time.Second):
+		_, leader := GetLeader()
+		if leader == unknown_leader {
+			kill_all_server(cmd)
+			panic("leader not elected")
+		}
 
 	}
-
 	return
 
 }
 
+func GetLeader() (uint64, uint64) {
+
+	select {
+	case <-time.After(2 * time.Second):
+		term, leader, err := rpc_call()
+		if err != nil || leader == 0 {
+			return GetLeader()
+
+		} else {
+
+			//log.Println("New leader is",leader)
+			return term, leader
+		}
+
+	}
+
+}
+
 //rpc call to ask latest term and current leader to all available servers
-func rpc_call() (uint64, uint64) {
-	var term_Leader uint64
-	term_Leader = unknown
-	var pid_Leader uint64
-	pid_Leader = unknown
+func rpc_call() (uint64, uint64, error) {
+	var term uint64
+	term = unknown
+	var leader uint64
+	leader = unknown_leader
 	var reply []LeaderInfo
 	port := 12345
-	reply = make([]LeaderInfo, 5)
-	for i := 1; i < 6; i++ {
+	reply = make([]LeaderInfo, nos)
+	for i := 1; i < nos+1; i++ {
 
 		client, err := rpc.Dial("tcp", "127.0.0.1:"+strconv.Itoa(port+i))
 		if err != nil {
@@ -93,17 +115,18 @@ func rpc_call() (uint64, uint64) {
 
 			err = client.Call("RaftTest.LeaderInfo", arg, &reply[i-1])
 			if err != nil {
-				log.Fatal("communication error:", err)
+				return unknown, unknown_leader, err
 			}
-			if term_Leader == unknown || pid_Leader == unknown {
-				term_Leader = reply[i-1].Term
-				pid_Leader = reply[i-1].Pid
+			if term == unknown || leader == unknown {
+				term = reply[i-1].Term
+				leader = reply[i-1].Pid
 
 			} else {
-				if term_Leader != reply[i-1].Term || pid_Leader != reply[i-1].Pid {
-					fmt.Println(term_Leader, reply[i-1].Term, pid_Leader, reply[i-1].Pid)
+				if leader != reply[i-1].Pid {
+					fmt.Println(term, reply[i-1].Term, leader, reply[i-1].Pid)
 					// kill_all_server(cmd)
-					fmt.Println("diffrent term or leader exist")
+					//fmt.Println("diffrent leader exist")
+					return unknown, unknown_leader, errors.New("leader not exist")
 
 				}
 
@@ -113,19 +136,19 @@ func rpc_call() (uint64, uint64) {
 		}
 
 	}
-	return term_Leader, pid_Leader
+	return term, leader, nil
 
 }
 
 func KillServer(wg *sync.WaitGroup, cmd []*exec.Cmd, j int, restart bool) {
 	path := GetPath() + "/bin/main"
-	fmt.Println("kill server ", j+1)
+	//fmt.Println("kill leader ",j+1)
 	cmd[j].Process.Kill()
 	cmd[j].Wait()
 	wg.Done()
 	if restart {
 		select {
-		case <-time.After(5 * time.Second):
+		case <-time.After(2 * time.Second):
 			cmd[j] = exec.Command(path, "-id", strconv.Itoa(j+1))
 			cmd[j].Start()
 		}
@@ -135,14 +158,14 @@ func KillServer(wg *sync.WaitGroup, cmd []*exec.Cmd, j int, restart bool) {
 //start server and check whether leader has elected in next 2sec.
 func TestElection_2(t *testing.T) {
 	fmt.Println("test 2")
-	nos = 5
+
 	wg = new(sync.WaitGroup)
 
-	select {
-	case <-time.After(2 * time.Second):
+	_, killed_Leader, err := rpc_call()
+	if err != nil {
+		kill_all_server(cmd)
+		panic("test failed")
 	}
-
-	_, killed_Leader := rpc_call()
 
 	//fmt.Println("Kill Leader", killed_Leader)
 
@@ -151,23 +174,20 @@ func TestElection_2(t *testing.T) {
 	wg.Wait()
 	var new_Leader uint64
 	new_Leader = unknown
+	_, new_Leader = GetLeader()
+	if new_Leader == killed_Leader {
+		fmt.Println(new_Leader, " ", killed_Leader)
+		kill_all_server(cmd)
+		panic("no leader not elected")
+
+	}
+
+	path := GetPath() + "/bin/main"
+	cmd[int(killed_Leader)-1] = exec.Command(path, "-id", strconv.Itoa(int(killed_Leader)))
+	cmd[int(killed_Leader)-1].Start()
 	select {
 	case <-time.After(2 * time.Second):
 
-		_, new_Leader = rpc_call()
-
-		if new_Leader == killed_Leader {
-			fmt.Println(new_Leader, " ", killed_Leader)
-			kill_all_server(cmd)
-			panic("no leader not elected")
-
-		}
-	}
-	path := GetPath() + "/bin/main"
-	select {
-	case <-time.After(1 * time.Second):
-		cmd[int(killed_Leader)-1] = exec.Command(path, "-id", strconv.Itoa(int(killed_Leader)))
-		cmd[int(killed_Leader)-1].Start()
 	}
 
 	return
@@ -180,61 +200,36 @@ func TestElection_3(t *testing.T) {
 	nos = 5
 	wg = new(sync.WaitGroup)
 	for i := 0; i < 3; i++ {
-		select {
-		case <-time.After(7 * time.Second):
-		}
 
-		_, killed_Leader := rpc_call()
+		_, killed_Leader := GetLeader()
 
 		//		fmt.Println("Kill Leader", killed_Leader)
 
 		wg.Add(1)
 		KillServer(wg, cmd, int(killed_Leader-1), false)
 		wg.Wait()
-		var new_Leader uint64
-		new_Leader = unknown
-		select {
-		case <-time.After(7 * time.Second):
 
-			_, new_Leader = rpc_call()
-
-			if new_Leader == killed_Leader {
-				fmt.Println(new_Leader, " ", killed_Leader)
-				kill_all_server(cmd)
-				panic("no leader not elected")
-
-			}
-		}
 	}
+
 	select {
 	case <-time.After(1 * time.Second):
-		final_term, final_Leader := rpc_call()
-		if final_Leader == unknwon_leader {
-			fmt.Println("Term", final_term, "Leader", final_Leader)
-		} else {
+		final_term, final_Leader, err := rpc_call()
+		if err != nil {
+			fmt.Println(final_term, final_Leader)
 			kill_all_server(cmd)
 			panic("leader still exist")
 		}
+
 	}
-	/*path := GetPath() + "/bin/main"
-	select {
-	case <-time.After(1 * time.Second):
-		cmd[int(killed_Leader)-1] = exec.Command(path, "-id", strconv.Itoa(int(killed_Leader)))
-		cmd[int(killed_Leader)-1].Start()
-	}
-	*/
-	select {
-	case <-time.After(2 * time.Second):
-		kill_all_server(cmd)
-	}
+
+	kill_all_server(cmd)
 	return
 
 }
 
 func kill_all_server(cmd []*exec.Cmd) {
-	for i := 1; i < 6; i++ {
+	for i := 1; i < nos+1; i++ {
 		cmd[i-1].Process.Kill()
 		cmd[i-1].Wait()
 	}
-
 }
